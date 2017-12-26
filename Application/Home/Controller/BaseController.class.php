d1e8a78729c56570af88055747d49064e06dcd45
wangjingye <4893278@qq.com>
2017-12-26 15:54:08 +0800

    订单已存在就返回正常

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -136,6 +136,7 @@ class BaseController extends Controller
                     $this->soapClient = new \SoapClient($this->platformWsdlConf['wsdl']);
                 }
             }
+            libxml_disable_entity_loader(false);
             $this->soapClient->__setLocation($this->platformWsdlConf['wsdl']);
             $result = $this->soapClient->$function($arg);
             $result = json_decode(json_encode($result), true);
